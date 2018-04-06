84a7b7d0c134c527d1327d703182e0ccee7d6e59
Bohdan Stupak <bstupak@acceptic.com>
2018-04-06 13:54:11 +0300

    Added parallelism
    Added parallelism with max degree contraint
    
    Added parallelism

--- SokkerPlayersCrawler/Program.fs
+++ SokkerPlayersCrawler/Program.fs
@@ -1,15 +1,18 @@
-﻿open FSharp.Data
+﻿open FSharp.Collections.ParallelSeq
+open FSharp.Data
 open System
 open System.Text.RegularExpressions
 open System.IO
 
 type Player = HtmlProvider<"http://sokker.org/player/PID/25410835">
+type Message = string * AsyncReplyChannel<string>
 
 let minPlayerId = 29657066
 let maxPlayerId = 29641529
 let minPrice = 1391000
 let priceRegex = Regex("[0-9 ]{1,}")
 let fileName = "output.txt"
+let maxDop = 4
     
 let extracrPrice input =
     let matched = priceRegex.Match input
@@ -19,10 +22,21 @@ let extracrPrice input =
         Regex.Replace(value, @"\s+", "") |> int            
     else
         0
-   
+
+let fileWriterAgent = MailboxProcessor<Message>.Start(fun inbox ->
+    let rec messageLoop() = async {
+        let! (url, replyChannel) = inbox.Receive()
+        File.AppendAllLines(fileName, [url])
+        replyChannel.Reply("recieved")
+        return! messageLoop()
+    }
+    messageLoop()    
+)
+    
 let load() = 
     [|maxPlayerId..minPlayerId|]
-    |> Array.iter(fun i -> 
+    |> PSeq.withDegreeOfParallelism maxDop
+    |> PSeq.iter(fun i -> 
         try
             let url = String.Format("http://sokker.org/player/PID/{0}", i)                        
             let player = Player.Load(url).Lists.List4
@@ -32,7 +46,8 @@ let load() =
             then 
                 if extracrPrice priceContainer > minPrice
                 then
-                    File.AppendAllLines(fileName, [url])
+                    let reply = fileWriterAgent.PostAndReply (fun replyChannel -> url, replyChannel)
+                    ()
             else
                 ()
         with
